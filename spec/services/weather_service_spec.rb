1a085af0bd18ae8aa74a0da42f8c692c11115e8d
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-25 22:02:41 -0300

    fix error in lint

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,16 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe 'weather_service_spec.rb' do
+RSpec.describe WeatherService do
   describe '#current', vcr: { cassette_name: 'weather/current', record: :new_episodes } do
     let(:location) { ['Sao Paulo', 'Tocantins', 'Minas Gerais'].sample }
 
     it 'success' do
-      expect(WeatherService.current(location)).not_to be_empty
+      expect(described_class.current(location)).not_to be_empty
     end
 
     it 'error' do
-      expect(WeatherService.current(nil).keys).to eql(%i[code message])
+      expect(described_class.current(nil).keys).to eql(%i[code message])
     end
   end
 end
