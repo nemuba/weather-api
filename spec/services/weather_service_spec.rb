f73014dd7118416d610f69baf3e70c4425a3c40c
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-25 21:58:39 -0300

    fix error in tests

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,16 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe WeatherService do
+RSpec.describe 'weather_service_spec.rb' do
   describe '#current', vcr: { cassette_name: 'weather/current', record: :new_episodes } do
     let(:location) { ['Sao Paulo', 'Tocantins', 'Minas Gerais'].sample }
 
     it 'success' do
-      expect(described_class.current(location)).not_to be_empty
+      expect(WeatherService.current(location)).not_to be_empty
     end
 
     it 'error' do
-      expect(described_class.current(nil).keys).to eql(%i[code message])
+      expect(WeatherService.current(nil).keys).to eql(%i[code message])
     end
   end
 end
